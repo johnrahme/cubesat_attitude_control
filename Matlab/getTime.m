2f46b82d01c5a2918dcb9bc74a31041b18351d7a
John Rahme <john.rahme.se@gmail.com>
2017-03-31 10:59:58 +0200

    Simulation change

---
+++ Matlab/getTime.m
@@ -0,0 +1,25 @@
+function [t1,t2,rpm] = getTime(deg)
+Iw = 1.46*10^(-5);
+Is = 1.67*10^(-3);
+
+%Ta = 0.00196; % Torque in Nm
+Ta = 0.004530672;
+%Td = 0.00196; %Torque in Nm
+
+Td = 0.03334261;
+
+a = Ta/Iw; %Max angular acceleration
+
+d = Td/Iw; %Max angular decelleration
+
+rad = deg*(2*pi/360);
+
+rpm = 60/pi*(rad*Is/(2*Iw)*a*d/(a+d)).^(0.5);
+
+w = rpm/60*2*pi;
+
+t1 = w/a;
+
+t2 = w/d;
+
+end
